f361b30fe0dbf69cb3355b53914735a0e3247350
Angad Gupta <i_am_angad@hotmail.com>
2023-03-24 10:24:18 -0400

    final push

---
+++ README.md
@@ -0,0 +1,19 @@
+# SSN
+
+Simple Sample Social Network
+
+#project initialization
+
+npm init -y
+
+#Installing reuirements for the project
+
+npm install express
+
+npm install nodemon --save-dev
+
+npm install oidc-provider ejs
+
+npm install body-parser
+
+
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'SSN Autho Verification',
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+module.exports = router;
